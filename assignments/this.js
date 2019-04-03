a2a55d378b1b4dc9740df457f27bb88e91eef065
Roger Caballero <rogercp1234@yahoo.com>
2019-04-03 12:14:02 -0700

    completed 4 principle of this keyword

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,15 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. This binds to the window when it is not pointing to any other object.
+
+* 2. This implicitly bind to objects which means that it is automatic.
+
+* 3. The new keyword allows the this keyword to bind to an objects properties and methods to the left
+    of the dot when creating a new function.
+
+* 4. This can also be binded in an explicit manner with methods in order to manually
+    set which object the 'this' keyword binds to.
 *
 * write out a code example of each explanation above
 */
